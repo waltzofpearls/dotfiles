847134778326ca8045b7e6ae652161d18735b77e
Rollie Ma <rollie.ma@gmail.com>
2015-01-12 23:59:06 -0800

    Added installation scripts for centos/macosx/ubuntu

--- install_macosx.sh
+++ install_macosx.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#
+# Install homebrew
+#
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install wget
+
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -O $HOME/.git-prompt.sh
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -O $HOME/.git-completion.sh
 
