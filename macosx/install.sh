df4ac76ac6da219d7a0cad8671eb52d43cf3b442
Rollie Ma <rollie.ma@gmail.com>
2015-07-26 23:49:14 -0700

    Added autoenv .env file for macosx

--- macosx/install.sh
+++ macosx/install.sh
@@ -6,9 +6,11 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew install wget
+brew install autoenv
 
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -O $HOME/.git-prompt.sh
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -O $HOME/.git-completion.sh
 
 tee -a $HOME/.profile < .profile
+tee -a $HOME/.bashrc < .bashrc
 source $HOME/.profile
